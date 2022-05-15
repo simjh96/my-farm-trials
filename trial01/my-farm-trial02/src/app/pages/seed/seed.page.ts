091da0ca85f35757fc5bbd25f77f738bf0d2107a
YONSEI\simjh96 <simjh96@gmail.com>
2022-05-16 01:02:49 +0900

    done: pile seed circles next: improve `for statement` for only checking nearby to right/ cutoff overflow

--- trial01/my-farm-trial02/src/app/pages/seed/seed.page.ts
+++ trial01/my-farm-trial02/src/app/pages/seed/seed.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import {
   AfterViewInit,
   Component,
@@ -5,11 +6,10 @@ import {
   OnInit,
   Renderer2,
   RendererFactory2,
-  ViewChild
+  ViewChild,
 } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import gsap from 'gsap';
-
 
 @Component({
   selector: 'app-seed',
@@ -18,61 +18,211 @@ import gsap from 'gsap';
 })
 export class SeedPage implements AfterViewInit, OnInit {
   public balls = [];
+  public _balls = [];
   public newBall;
+  public _newBall;
   public featureBackground;
   public content;
   public i: number;
+  public vw;
+  public vh;
 
-  constructor(private renderer: Renderer2,
+  constructor(
+    private renderer: Renderer2,
     private el: ElementRef,
-    private platform: Platform) {
-    }
+    private platform: Platform
+  ) {
+    this.vw = this.platform.width();
+    this.vh = this.platform.height();
+  }
   ngOnInit(): void {
     this.featureBackground = this.renderer.createElement('ion-grid');
     this.content = this.el.nativeElement.children[1];
-    this.renderer.setProperty(this.featureBackground, 'id', 'featureBackground');
+    this.renderer.setProperty(
+      this.featureBackground,
+      'id',
+      'featureBackground'
+    );
     this.renderer.appendChild(this.content, this.featureBackground);
 
-    for (this.i = 0; this.i < 80; this.i++) {
+    for (this.i = 0; this.i < 20; this.i++) {
+      // meta data of balls
+      const r = this.vw * 0.15 * this.random(0.7, 1);
+      this._newBall = { x: r, y: 0, r };
+      this._balls.push(this._newBall);
+
+      // render balls
       this.newBall = this.renderer.createElement('ion-grid');
       this.renderer.addClass(this.newBall, 'ball');
+      this.renderer.setStyle(this.newBall, 'width', `${2 * r}px`);
+      this.renderer.setStyle(this.newBall, 'height', `${2 * r}px`);
+      this.renderer.appendChild(
+        this.newBall,
+        this.renderer.createText(`${this.i}`)
+      );
       this.renderer.appendChild(this.featureBackground, this.newBall);
       this.balls.push(this.newBall);
     }
+
+    this.calcInitPos();
   }
 
   ngAfterViewInit(): void {
-    console.log(this.random(-0.1, 1.1));
-    gsap.set(this.balls, {
-      backgroundColor: 'random([#663399,#84d100,#cc9900,#0066cc,#993333,#d8637c])',
-      scale: 'random(0.4, 1)',
-      x: `random(-100, ${this.platform.width()})`,
-      y: '-100vh'
+    this.balls.forEach((e, i) => {
+      gsap.set(e, {
+        backgroundColor:
+          'random([#663399,#84d100,#cc9900,#0066cc,#993333,#d8637c])',
+        x: this._balls[i]['x'],
+        y: '-50vw',
+        xPercent: -50,
+        yPercent: -50,
+      });
     });
-
-    gsap.to(this.balls.slice(60,80), {
-      duration: 0.5,
-      delay: "random(0.5, 1.2)",
-      y: '51vh',
-    });
-    gsap.to(this.balls.slice(40,60), {
-      duration: 0.5,
-      delay: "random(0.8, 1.3)",
-      y: '34vh',
-    });
-    gsap.to(this.balls.slice(20,40), {
-      duration: 0.5,
-      delay: "random(0.9, 1.4)",
-      y: '17vh',
-    });
-    gsap.to(this.balls.slice(0,20), {
-      duration: 0.5,
-      delay: "random(1.3, 1.5)",
-      y: '0vh',
+    this.balls.forEach((e, i) => {
+      gsap.to(e, {
+        xPercent: -50,
+        yPercent: -50,
+        delay: 0.5 + 0.05 * i,
+        y: this._balls[i]['y'],
+      });
     });
   }
 
   random(min, max) {
     return Math.random() * (max - min + 1) + min;
+  }
+
+  calcInitPos() {
+    console.log(`==================calcInitPos works==================`);
+    for (let i = 0; i < this._balls.length; i++) {
+      const element = this._balls[i];
+      this.setPos(i);
+    }
+  }
+
+  setPos(upto: number) {
+    // set pos upto i
+    if (upto === 0) {
+      this._balls[0]['y'] = this.vh * 0.7 - this._balls[0]['r'];
+    } else {
+      // find inittouch idx
+      let initTouch: number = this.getInitTouch(upto);
+      // roll
+      // pile up
+      let d = this.getD(initTouch, upto);
+      this._balls[upto]['y'] =
+        this._balls[initTouch]['y'] -
+        Math.sqrt(
+          d ** 2 - (this._balls[initTouch]['x'] - this._balls[upto]['x']) ** 2
+        );
+      console.log(`piled to :`, this._balls[upto]['y']);
+
+      if (this._balls[upto]['x'] < this._balls[initTouch]['x']) {
+        // not rollable
+        console.log(`not rollable`);
+      } else {
+        console.log(`rollable`);
+        let theta = this.getTheta(upto, initTouch);
+        console.log(`current theta`, theta);
+        // roll
+        let banList = [initTouch];
+        let roll = this.rollTilTouch(upto, initTouch, theta, banList);
+        console.log(`banlist`, banList);
+        let safeCount = 0;
+        while (roll['ctn'] === true && safeCount < 10) {
+          roll = this.rollTilTouch(
+            upto,
+            roll['tIdx'],
+            this.getTheta(upto, roll['tIdx']),
+            banList
+          );
+          safeCount += 1;
+        }
+        console.log(
+          `ball ${upto} has finnished at ${this._balls[upto]['x']}, ${this._balls[upto]['y']}`
+        );
+      }
+    }
+  }
+
+  getInitTouch(upto) {
+    let candidates = [];
+    for (let i = 0; i < upto; i++) {
+      let y =
+        -Math.sqrt(
+          (this._balls[upto]['r'] + this._balls[i]['r']) ** 2 -
+            (this._balls[upto]['r'] - this._balls[i]['x']) ** 2
+        ) + this._balls[i]['y'];
+      candidates.push({ i, y: isNaN(y) ? this.vh : y });
+    }
+    candidates.forEach((v, i) => {
+      console.log(`point ${upto} <-${candidates[i]['y']}-> ${i}`);
+    });
+    return candidates.reduce(
+      (past: { i: number; y: number }, curr: { i: number; y: number }) =>
+        curr['y'] <= past['y'] ? curr : past,
+      { i: -1, y: this.vh }
+    )['i'];
+  }
+  rollTilTouch(
+    upto,
+    initTouch,
+    theta,
+    banList: Array<number>
+  ): { tIdx: number; ctn: boolean; theta: number } {
+    let d = this.getD(upto, initTouch);
+    let touched = false;
+    let safeCount = 0;
+
+    while (
+      this._balls[upto]['x'] + this._balls[upto]['r'] < this.vw * 1.5 &&
+      this._balls[upto]['y'] + this._balls[upto]['r'] < this.vh * 0.7 &&
+      !touched &&
+      safeCount < 100
+    ) {
+      theta -= 0.1;
+      this._balls[upto]['x'] =
+        this._balls[initTouch]['x'] + d * Math.cos(theta);
+      this._balls[upto]['y'] =
+        this._balls[initTouch]['y'] - d * Math.sin(theta);
+      console.log(
+        `==== tilted ====`,
+        this._balls[upto]['x'],
+        this._balls[upto]['y']
+      );
+      console.log(`current theta : `, theta);
+      for (let i = 0; i < upto; i++) {
+        if (
+          !banList.includes(i) &&
+          Math.hypot(
+            this._balls[upto]['x'] - this._balls[i]['x'],
+            this._balls[upto]['y'] - this._balls[i]['y']
+          ) <=
+            this._balls[upto]['r'] + this._balls[i]['r']
+        ) {
+          console.log(`${upto} detacted collision with ${i} `);
+          touched = true;
+          banList.push(i);
+          return {
+            tIdx: i,
+            ctn: this._balls[upto]['x'] >= this._balls[i]['x'],
+            theta,
+          };
+        }
+      }
+      safeCount += 1;
+    }
+    console.log(`ended with touching wall`);
+    return { tIdx: 0, ctn: false, theta };
+  }
+  getTheta(i, j) {
+    // get theta
+    return Math.atan(
+      -(this._balls[i]['y'] - this._balls[j]['y']) /
+        (this._balls[i]['x'] - this._balls[j]['x'])
+    );
+  }
+  getD(i, j) {
+    return this._balls[i]['r'] + this._balls[j]['r'];
   }
 }
